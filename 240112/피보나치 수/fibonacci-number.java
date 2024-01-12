67706ebb5b5b63f27177ea96d9201f99fef4cbda
백자민(Baik Jamin) <78345851+sdflksjf2683@users.noreply.github.com>
2024-01-12 23:02:24 +0900

    Title: 피보나치 수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240112/피보나치 수/fibonacci-number.java
+++ 240112/피보나치 수/fibonacci-number.java
@@ -5,15 +5,15 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
 
-        int[] dp = new int[N];
+        int[] dp = new int[N+1];
 
-        dp[0] = 1;
         dp[1] = 1;
+        dp[2] = 1;
 
-        for(int i=2;i<N;i++) {
+        for(int i=3;i<=N;i++) {
             dp[i] = dp[i-1] + dp[i-2];
         }
 
-        System.out.println(dp[N-1]);
+        System.out.println(dp[N]);
     }
 }
