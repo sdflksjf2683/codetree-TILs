98ab892ee53f1afabb890a1bec2df570af2db5aa
백자민(Baik Jamin) <78345851+sdflksjf2683@users.noreply.github.com>
2024-01-12 22:02:20 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 89ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240112/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.java
+++ 240112/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.java
@@ -10,9 +10,7 @@ public class Main {
     static int solve(int n) {
         for(int i=1;i<N;i++) {
             for(int j=1;j<N;j++) {
-                if(map[i][j]<n) {
-                    map[i][j] = Integer.MAX_VALUE;
-                }
+                if(map[i][j]<n) continue;
                 dp[i][j] = Math.max(Math.min(dp[i-1][j], dp[i][j-1]), map[i][j]);
             }
         }
