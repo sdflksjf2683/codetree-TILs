ee24a9a287f6e3b50936a98565d2cf902ff4ace8
백자민(Baik Jamin) <78345851+sdflksjf2683@users.noreply.github.com>
2024-01-12 22:01:38 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 93ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240112/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.java
@@ -0,0 +1,63 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int N;
+
+    static int[][] map,dp;
+
+    static int solve(int n) {
+        for(int i=1;i<N;i++) {
+            for(int j=1;j<N;j++) {
+                if(map[i][j]<n) {
+                    map[i][j] = Integer.MAX_VALUE;
+                }
+                dp[i][j] = Math.max(Math.min(dp[i-1][j], dp[i][j-1]), map[i][j]);
+            }
+        }
+
+        return dp[N-1][N-1];
+    }
+
+    static void init() {
+        for(int i=0;i<N;i++) {
+            Arrays.fill(dp[i], Integer.MAX_VALUE);
+        }
+
+        dp[0][0] = map[0][0];
+
+        for(int n=1;n<N;n++) {
+            dp[n][0] = Math.max(dp[n-1][0], map[n][0]);
+            dp[0][n] = Math.max(dp[0][n-1], map[0][n]);
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        map = new int[N][N];
+        dp = new int[N][N];
+
+        StringTokenizer st;
+        for(int i=0;i<N;i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j=0;j<N;j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        } //end input
+
+        int ans = Integer.MAX_VALUE;
+        //최소값을 정해두고 dp 채워보기
+        for(int n=1;n<=100;n++) {
+            init();
+            int tmp = solve(n);
+
+            ans = Math.min(ans, tmp-n);
+        }
+
+        System.out.print(ans);
+    }
+}
