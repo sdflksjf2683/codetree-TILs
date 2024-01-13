7d2ec700e9d8013c60b057d27efe521be22e0a19
백자민(Baik Jamin) <78345851+sdflksjf2683@users.noreply.github.com>
2024-01-13 23:07:45 +0900

    Title: 사각형 채우기 3, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

--- 240113/사각형 채우기 3/rectangle-fill-3.java
+++ 240113/사각형 채우기 3/rectangle-fill-3.java
@@ -8,16 +8,16 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        int[] dp = new int[n+1];
+        long[] dp = new long[n+1];
 
+        dp[0] = 1;
         dp[1] = 2;
-        if(n==1) {
-            System.out.println(dp[n]);
-            return;
-        }
-        dp[2] = 7;
-        for(int i=3;i<=n;i++) {
-            dp[i] = dp[i-1]*3+1;
+        for(int i=2;i<=n;i++) {
+            dp[i] = (dp[i-1]*2 + dp[i-2]*3)%MOD;
+
+            for(int j=i-3;j>=0;j--) {
+                dp[i] = (dp[i]+dp[j]*2)%MOD;
+            }
         }
 
         System.out.println(dp[n]);
