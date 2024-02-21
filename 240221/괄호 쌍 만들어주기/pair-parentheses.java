94bdba7c734f7c4edb71b32862ede73529aa1415
백자민(Baik Jamin) <78345851+sdflksjf2683@users.noreply.github.com>
2024-02-21 17:59:45 +0900

    Title: 괄호 쌍 만들어주기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240221/괄호 쌍 만들어주기/pair-parentheses.java
+++ 240221/괄호 쌍 만들어주기/pair-parentheses.java
@@ -4,19 +4,23 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         char[] inputs = br.readLine().toCharArray();
+        int N = inputs.length;
 
-        int lcnt=0,rcnt=0;
-        char tmp = inputs[0];
-        for(int i=1,size=inputs.length;i<size;i++) {
-            if(tmp==inputs[i]) {
-                if(tmp=='(') lcnt++;
-                else rcnt++;
-            } else {
-                tmp = inputs[i];
-            }
+        int[] dp = new int[N];
+        for(int i=1;i<N;i++) {
+            dp[i] = dp[i-1];
 
+            if(inputs[i-1]=='(' && inputs[i]=='(')
+                dp[i]++;
         }
 
-        System.out.println(lcnt*rcnt);
+        long ans=0;
+        for(int i=N-1;i>=1;i--) {
+            if(inputs[i]==')' && inputs[i-1]==')') {
+                ans += dp[i-2];
+            }
+        }
+        
+        System.out.println(ans);
     }
 }
