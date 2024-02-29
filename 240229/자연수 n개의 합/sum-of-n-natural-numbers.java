24a5c264e13a1a7545622277256ba7d7d9a0ce19
백자민(Baik Jamin) <78345851+sdflksjf2683@users.noreply.github.com>
2024-02-29 18:41:19 +0900

    Title: 자연수 n개의 합, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

--- 240229/자연수 n개의 합/sum-of-n-natural-numbers.java
+++ 240229/자연수 n개의 합/sum-of-n-natural-numbers.java
@@ -5,13 +5,14 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         long S = sc.nextLong();
 
-        long l=1, r=S, mid=0, max=0;
+        long l=1, mid=0, max=0;
+        long r = (long)Math.sqrt(S*2)+1;
 
         while(l<=r) {
             mid = (l+r)/2;
             
             if(mid*(mid+1)/2 <= S) {
-                max = Math.max(max, l);
+                max = Math.max(max, mid);
                 l = mid+1;
             } else {
                 r = mid-1;
