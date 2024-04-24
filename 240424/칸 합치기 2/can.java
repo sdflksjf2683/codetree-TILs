333a4f60438eb5606fa3355d945fc3a5c3c8113f
백자민(Baik Jamin) <78345851+sdflksjf2683@users.noreply.github.com>
2024-04-24 21:46:27 +0900

    Title: 칸 합치기 2, Time: 3022ms, Memory: 14MB, Status: Time Limit Exceed - CodeTree

---
+++ 240424/칸 합치기 2/can.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int N,M;
+
+    static int[] parent;
+
+    static int find(int x) {
+        if(parent[x]==x) {
+            return x;
+        }
+
+        return find(parent[x]);
+    }
+
+    static void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+
+        if(x!=y) {
+            parent[x] = y;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        parent = new int[N+1];
+        for(int i=1;i<=N;i++) {
+            parent[i] = i;
+        } //init
+
+        StringBuilder sb = new StringBuilder();
+        for(int m=0;m<M;m++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            for(int i=a;i<b;i++) {
+                if(parent[i]!=parent[b]) {
+                    N--;
+                    union(i,b);
+                }
+            }
+            sb.append(N+"\n");
+        }
+
+        System.out.println(sb.toString());
+    }
+}
