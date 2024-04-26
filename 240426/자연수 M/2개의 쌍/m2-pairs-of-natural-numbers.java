0220849106010426a7733d2919fde7b9a5f7c072
백자민(Baik Jamin) <78345851+sdflksjf2683@users.noreply.github.com>
2024-04-26 20:33:01 +0900

    Title: 자연수 M/2개의 쌍, Time: 229ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240426/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.java
+++ 240426/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.java
@@ -27,20 +27,23 @@ public class Main {
         int l=0,r=N-1;
         //두 자연수의 합을 가장 적게 만드는 법 = 가장 큰 수와 가장 작은 수를 더하기(중간 수에 도달할 때까지 반복)
         while(l<=r) {
-            //해당 숫자를 모두 소진했다면 다른 숫자 이용
-            if(nums[l][1]==0) {
-                l++;
-                continue;
-            }
-            if(nums[r][1]==0) {
-                r--;
-                continue;
-            }
 
             C = Math.max(C, nums[l][0]+nums[r][0]);
-            //숫자를 사용했으므로 개수 감소
-            nums[l][1]--;
-            nums[r][1]--;
+
+            //두 숫자의 남은 개수에 차이가 있는 경우
+            //더 적은 숫자의 개수를 더 많은 숫자의 개수에서 빼주고 더 적은 쪽 포인터를 옮김(어차피 같은 값이므로 스킵 가능)
+            if(nums[l][1]<nums[r][1]) {
+                l++;
+                nums[r][1] -= nums[l][1]; 
+                continue;
+            } else if(nums[l][1]>nums[r][1]) {
+                r--;
+                nums[l][1] -= nums[r][1];
+                continue;
+            } else { //개수가 동일하다면 포인터 둘 다 이동
+                l++;
+                r--;
+            }
         }
         System.out.println(C);
     }
