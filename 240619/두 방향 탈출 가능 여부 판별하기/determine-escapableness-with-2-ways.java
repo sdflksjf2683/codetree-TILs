952def71f3a37d7e97d870ff7d87650bfa3967c7
백자민(Baik Jamin) <78345851+sdflksjf2683@users.noreply.github.com>
2024-06-19 18:20:57 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 3048ms, Memory: 9MB, Status: Time Limit Exceed - CodeTree

---
+++ 240619/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.java
@@ -0,0 +1,56 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int N,M,ans;
+
+    static int[][] map;
+
+    static int[] di = {1,0};
+    static int[] dj = {0,1};
+
+    static void dfs(int ti, int tj) {
+        if(ti==N-1 && tj==M-1) {
+            ans = 1;
+            return;
+        }
+
+        if(ans==1) {
+            return;
+        }
+
+        for(int d=0;d<2;d++) {
+            int ni = ti+di[d];
+            int nj = tj+dj[d];
+
+            if(ni<0 || ni>=N || nj<0 || nj>=M) continue;
+            if(map[ni][nj]==0) continue;
+
+            dfs(ni,nj);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        map = new int[N][M];
+
+        for(int i=0;i<N;i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j=0;j<M;j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        } //end input
+
+        ans = 0;
+
+        dfs(0,0);
+
+        System.out.println(ans);
+    }
+}
