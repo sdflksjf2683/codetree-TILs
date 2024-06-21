7ea973debab5badc9ab8072069b419bcaa18618e
백자민(Baik Jamin) <78345851+sdflksjf2683@users.noreply.github.com>
2024-06-21 15:04:03 +0900

    Title: 계단 오르기 2, Time: 110ms, Memory: 9MB, Status: Passed - CodeTree

--- 240621/계단 오르기 2/climbing-stairs-2.java
+++ 240621/계단 오르기 2/climbing-stairs-2.java
@@ -14,15 +14,23 @@ public class Main {
         }//end input
 
         int[][] dp = new int[N+1][4]; //i: 계단 층 수, j:지금까지 1계단 오른 횟수
+        
+        //init
+        for(int i=0;i<=N;i++) {
+            Arrays.fill(dp[i], -1);
+        }
         dp[0][0] = 0; //초기상태 동전 개수 = 0개
         dp[1][1] = coins[1]; //1계단 오른 경우 = 1층에 있는 동전을 얻음
 
         for(int i=2;i<=N;i++) {
 
-            dp[i][0] = dp[i-2][0] + coins[i]; //두칸 올라서 동전을 얻은 경우
+            dp[i][0] = dp[i-2][0]<0?dp[i][0]:dp[i-2][0]+coins[i]; //두칸 올라서 동전을 얻은 경우
 
             for(int j=1;j<4;j++) { //1칸 오르기를 사용한 경우
-                dp[i][j] = Math.max(dp[i-1][j-1], dp[i-2][j])+coins[i]; //2칸을 오른 경우와 비교해서 최적의 상태 저장
+                dp[i][j] = Math.max(dp[i-1][j-1], dp[i-2][j]); //2칸을 오른 경우와 비교해서 최적의 상태 저장
+                if(dp[i][j]>-1) {
+                    dp[i][j] += coins[i];
+                }
             }
         }
 
