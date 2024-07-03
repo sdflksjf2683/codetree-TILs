83c735e7ae903544ada5bd68c2172f99f5518020
백자민(Baik Jamin) <78345851+sdflksjf2683@users.noreply.github.com>
2024-07-03 18:24:36 +0900

    Title: 가장 많이 겹치는 구간 2, Time: 608ms, Memory: 29MB, Status: Passed - CodeTree

--- 240703/가장 많이 겹치는 구간 2/section-with-maximum-overlap-2.java
+++ 240703/가장 많이 겹치는 구간 2/section-with-maximum-overlap-2.java
@@ -6,23 +6,44 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        int[] arr = new int[100_001]; 
+        PriorityQueue<Point> pq = new PriorityQueue<>();
         StringTokenizer st;
         for(int i=0;i<N;i++) {
             st = new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
 
-            arr[s]++;
-            arr[e]--;
+            pq.offer(new Point(s, 1)); //시작점
+            pq.offer(new Point(e, -1)); //끝점
         }
 
-        long ans=0, sum=0;
-        for(int i=1;i<=100_000;i++) {
-            sum+=arr[i];
+        int ans=0, sum=0;
+        while(!pq.isEmpty()) {
+            Point tmp = pq.poll();
+
+            sum += tmp.w;
+
             ans = Math.max(ans, sum);
         }
 
         System.out.println(ans);
+    }
+
+    static class Point implements Comparable<Point> {
+        int idx, w;
+
+        public Point(int idx, int w) {
+            this.idx = idx;
+            this.w = w;
+        }
+
+        @Override
+        public int compareTo(Point p) {
+            if(this.idx == p.idx) {
+                return this.w-p.w;
+            }
+
+            return this.idx-p.idx;
+        }
     }
 }
