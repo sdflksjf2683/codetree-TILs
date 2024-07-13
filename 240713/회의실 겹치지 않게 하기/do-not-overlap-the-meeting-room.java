b486fdca1d2ba394885abfddaeeee942bc63500b
백자민(Baik Jamin) <78345851+sdflksjf2683@users.noreply.github.com>
2024-07-13 15:56:41 +0900

    Title: 회의실 겹치지 않게 하기, Time: 561ms, Memory: 23MB, Status: Passed - CodeTree

--- 240713/회의실 겹치지 않게 하기/do-not-overlap-the-meeting-room.java
+++ 240713/회의실 겹치지 않게 하기/do-not-overlap-the-meeting-room.java
@@ -21,20 +21,20 @@ public class Main {
             pq.offer(new Meeting(s,e));
         } //end input
 
-        int cnt=0,next=pq.poll().s; //cnt=제외할 회의 개수, next=다음 회의 시작 시간
+        int cnt=0,prev=pq.poll().e; //cnt=제외할 회의 개수, prev=직전 회의 종료 시간
         while(!pq.isEmpty()) {
             Meeting tmp = pq.poll();
 
-            //현재 회의 끝나는 시간이 다음 회의 시작 시간보다 늦는 경우
+            //직전 회의 종료 시간이 현재 회의 시작시간보다 늦는 경우
             //현재 회의는 포기해야 한다(회의 진행 불가능)
-            if(tmp.e>next) {
+            if(tmp.s<prev) {
                 cnt++;
                 continue;
             }
 
             //회의 진행이 가능할 경우
-            //회의 시작 시간 갱신
-            next = tmp.s;
+            //회의 종료 시간 갱신
+            prev = tmp.e;
         }
 
         System.out.println(cnt);
@@ -50,11 +50,11 @@ public class Main {
 
         @Override
         public int compareTo(Meeting m) {
-            if(this.e==m.e) { //종료 시간이 같다면 총 회의 시간이 짧은 회의를 우선 선택(더 많은 회의를 진행하기 위함)
-                return m.s-this.s;
+            if(this.e==m.e) { //종료 시간이 같다면 시작 시간이 빠른 회의가 높은 우선순위를 가짐
+                return this.s-m.s;
             }
 
-            return m.e-this.e; //종료 시간이 더 늦은 회의가 높은 우선순위를 가짐
+            return this.e-m.e; //종료 시간이 더 빠른 회의가 높은 우선순위를 가짐
         }
     }
 }
