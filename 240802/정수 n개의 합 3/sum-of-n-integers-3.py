2c233e641ab71e961073ecba38115c86d0c3857a
백자민(Baik Jamin) <78345851+sdflksjf2683@users.noreply.github.com>
2024-08-02 18:47:29 +0900

    Title: 정수 n개의 합 3, Time: 182ms, Memory: 27MB, Status: Passed - CodeTree

--- 240802/정수 n개의 합 3/sum-of-n-integers-3.py
+++ 240802/정수 n개의 합 3/sum-of-n-integers-3.py
@@ -8,23 +8,16 @@ board = [list(map(int, input().split())) for _ in range(N)]
 
 #누적합 배열 만들기
 prefix = [[0 for _ in range(N+1)] for _ in range(N+1)]
-# prefix[1][1] = board[0][0]
-# for i in range(1, N):
-#     prefix[i][0] = prefix[i-1][0] + board[i][0]
-
-# for j in range(1, N):
-#     prefix[0][j] = prefix[0][j-1] + board[0][j-1]
-
 for i in range(1, N+1):
     for j in range(1, N+1):
         prefix[i][j] = prefix[i-1][j]+prefix[i][j-1]-prefix[i-1][j-1]+board[i-1][j-1]
 
-def calc(i, j):
-    return prefix[i][j]-prefix[i-K][j]-prefix[i][j-K]+prefix[i-K][j-K]
 
+#K*K 크기의 사각형으로 만드는 값 중 가장 큰 값 찾기
 ans = 0
-for i in range(1, N-K+2):
-    for j in range(1, N-K+2):
-        ans = max(ans,calc(i,j))
+for i in range(K, N+1):
+    for j in range(K, N+1):
+        tmp = prefix[i][j] - prefix[i-K][j] - prefix[i][j-K] + prefix[i-K][j-K]
+        ans = max(ans, tmp)
 
 print(ans)
