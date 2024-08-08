eb655fa66fc2fea9fab5316b547bd8421b513200
백자민(Baik Jamin) <78345851+sdflksjf2683@users.noreply.github.com>
2024-08-08 23:14:27 +0900

    Title: 겹치는 숫자가 없는 최대 구간, Time: 165ms, Memory: 31MB, Status: Passed - CodeTree

--- 240808/겹치는 숫자가 없는 최대 구간/max-interval-without-overlapping-numbers.py
+++ 240808/겹치는 숫자가 없는 최대 구간/max-interval-without-overlapping-numbers.py
@@ -7,20 +7,16 @@ arr = list(map(int, input().split()))
 
 check = [0] * 100001 #구간 내 존재하는 숫자 개수를 저장하는 배열
 
-l,r = 0,1
-check[arr[l]]+=1
+l,r = 0,0
 ans = 1 #최소 구간 크기=1
 
-while l<=r and r<N:
-
-    tmp = arr[r]
-    check[tmp]+=1
-
-    if check[tmp]>1:
+while r<N:
+    if check[arr[r]]==0:
+        check[arr[r]]+=1
+        ans = max(ans, r-l+1)
+        r+=1
+    else:
         check[arr[l]]-=1
         l+=1
     
-    ans = max(ans, r-l+1)
-    r+=1
-
 print(ans)
